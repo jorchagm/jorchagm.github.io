54089d459d41e5b1bbc8b16ff4353a14978807ac
DianaPalau <36201129+DianaPalau@users.noreply.github.com>
2022-01-11 20:30:58 -0600

    Test 1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "48b5baadf72765bcf5f34ed94da81b4e",
-"index.html": "f84183b0a094ce880c0cf6628ddd60f7",
-"/": "f84183b0a094ce880c0cf6628ddd60f7",
-"main.dart.js": "b2d2f8259232fd17e559ec7078d43142",
+"index.html": "15fee871edbf244bf2e291846570073f",
+"/": "15fee871edbf244bf2e291846570073f",
+"main.dart.js": "52e4bc011fca16f8fd5d2f1286a842bb",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
